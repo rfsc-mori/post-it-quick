eab243c357689ef663f392038c55a4c2ac9c11b1
Rafael Coelho <rfsc.mori@gmail.com>
2024-06-06 20:13:27 +0000

    fix: corrige tratamento de erro interno no fluxo de login
    - Mostra mensagem "tente mais tarde"
    - Gera log de erros na aplicação
    
    Signed-off-by: Rafael Coelho <rfsc.mori@gmail.com>

--- src/exceptions/api/authentication/UnknownAuthenticationError.exception.ts
+++ src/exceptions/api/authentication/UnknownAuthenticationError.exception.ts
@@ -1,7 +1,8 @@
-import { UnauthorizedException } from '@nestjs/common';
+import { InternalServerErrorException } from '@nestjs/common';
+import { AUTHENTICATION_ERROR_MESSAGES } from 'messages/error/api/authentication/authenticationErrorMessages.constant';
 
-export class UnknownAuthenticationError extends UnauthorizedException {
-  constructor(error: Error) {
-    super(error);
+export class UnknownAuthenticationError extends InternalServerErrorException {
+  constructor() {
+    super(AUTHENTICATION_ERROR_MESSAGES.UNKNOWN_AUTHENTICATION_ERROR);
   }
 }
