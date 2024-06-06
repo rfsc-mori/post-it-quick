b4f15b973221d0f06ca1709e8c4a68f01acba3b0
Rafael Coelho <rfsc.mori@gmail.com>
2024-06-06 20:58:47 +0000

    feat: adiciona tabela de perfil de usuário
    - O perfil tem a descrição e o avatar do usuário.
    - A alteração de avatar será adicionada futuramente.
    
    Signed-off-by: Rafael Coelho <rfsc.mori@gmail.com>

---
+++ prisma/migrations/20240606165351_user_add_profile/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "UserProfile" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "description" TEXT,
+    "avatar_key" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "UserProfile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserProfile_user_id_key" ON "UserProfile"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "UserProfile" ADD CONSTRAINT "UserProfile_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
