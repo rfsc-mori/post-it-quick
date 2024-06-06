257407dc1fa187c0a82ce397f544584b583efa83
Rafael Coelho <rfsc.mori@gmail.com>
2024-06-06 21:59:00 +0000

    feat: apaga avatar do S3 ao apagar usuário
    Signed-off-by: Rafael Coelho <rfsc.mori@gmail.com>

--- src/modules/api/user/routes/delete-user/DeleteUser.service.ts
+++ src/modules/api/user/routes/delete-user/DeleteUser.service.ts
@@ -1,16 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ForbiddenAction } from 'exceptions/api/authorization/ForbiddenAction.exception';
 import { UserNotFoundException } from 'exceptions/api/user/UserNotFound.exception';
 import type { TRequestUser } from 'modules/api/authentication/types/requestUser.type';
 import { AuthorizationGrant } from 'modules/api/authorization/constants/authorizationGrant.constant';
 import { AUTHORIZATION_RESOURCE } from 'modules/api/authorization/constants/authorizationResource.constant';
 import { UserRepository } from 'modules/database/repository/user/UserRepository';
+import { UserProfileRepository } from 'modules/database/repository/user-profile/UserProfileRepository';
+import { S3Service } from 'modules/s3/S3.service';
 
 @Injectable()
 export class DeleteUserService {
+  private readonly logger = new Logger(DeleteUserService.name);
+
   constructor(
     private readonly authorization: AuthorizationGrant,
     private readonly user_repository: UserRepository,
+    private readonly s3: S3Service,
+    private readonly user_profile_repository: UserProfileRepository,
   ) {}
 
   async run(user: TRequestUser, target_id: string): Promise<void> {
@@ -20,6 +26,14 @@ export class DeleteUserService {
 
     if (!target_exists) {
       throw new UserNotFoundException();
+    }
+
+    const profile = await this.user_profile_repository.findByUserId(target_id);
+
+    if (profile?.avatar_key) {
+      await this.s3.deleteByKey(profile.avatar_key).catch((error: Error) => {
+        this.logger.error(error);
+      });
     }
 
     await this.user_repository.deleteById(target_id);
