74f13056a75e6db0b9489ca89f2e92cb01aef1e8
Rafael Coelho <rfsc.mori@gmail.com>
2024-06-06 20:44:27 +0000

    feat: adiciona decorators para configurar autorização em rotas
    - accessTokenWithAuthorization: Rota tem autorização com processamento manual
    
    - authorize: Rota tem autorização com processamento automático via guard
    
    Signed-off-by: Rafael Coelho <rfsc.mori@gmail.com>

---
+++ src/modules/api/authorization/decorators/accessTokenWithAuthorization.decorator.ts
@@ -0,0 +1,26 @@
+import { applyDecorators, UseGuards } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiForbiddenResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { AUTHENTICATION_ERROR_MESSAGES } from 'messages/error/api/authentication/authenticationErrorMessages.constant';
+import { AUTHORIZATION_ERROR_MESSAGES } from 'messages/error/api/authorization/authorizationErrorMessages.constant';
+import { AccessTokenAuthenticationGuard } from 'modules/api/authentication/guards/access-token-authentication/AccessTokenAuthentication.guard';
+
+export function accessTokenWithAuthorization(): MethodDecorator {
+  return applyDecorators(
+    UseGuards(AccessTokenAuthenticationGuard),
+
+    ApiBearerAuth(),
+
+    ApiUnauthorizedResponse({
+      description:
+        AUTHENTICATION_ERROR_MESSAGES.ACCESS_TOKEN_EXPIRED_OR_INVALID,
+    }),
+
+    ApiForbiddenResponse({
+      description: AUTHORIZATION_ERROR_MESSAGES.FORBIDDEN_ACTION,
+    }),
+  );
+}
---
+++ src/modules/api/authorization/decorators/authorize.decorator.ts
@@ -0,0 +1,31 @@
+import { applyDecorators, UseGuards } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiForbiddenResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { AUTHENTICATION_ERROR_MESSAGES } from 'messages/error/api/authentication/authenticationErrorMessages.constant';
+import { AUTHORIZATION_ERROR_MESSAGES } from 'messages/error/api/authorization/authorizationErrorMessages.constant';
+import { AccessTokenAuthenticationGuard } from 'modules/api/authentication/guards/access-token-authentication/AccessTokenAuthentication.guard';
+import { ACGuard, UseRoles } from 'nest-access-control';
+
+export function authorize(
+  ...roles: Parameters<typeof UseRoles>
+): MethodDecorator {
+  return applyDecorators(
+    UseGuards(AccessTokenAuthenticationGuard, ACGuard),
+
+    UseRoles(...roles),
+
+    ApiBearerAuth(),
+
+    ApiUnauthorizedResponse({
+      description:
+        AUTHENTICATION_ERROR_MESSAGES.ACCESS_TOKEN_EXPIRED_OR_INVALID,
+    }),
+
+    ApiForbiddenResponse({
+      description: AUTHORIZATION_ERROR_MESSAGES.FORBIDDEN_ACTION,
+    }),
+  );
+}
