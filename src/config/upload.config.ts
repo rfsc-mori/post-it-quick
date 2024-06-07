73e569e4139bf861e099925addd76802c8cad442
Rafael Coelho <rfsc.mori@gmail.com>
2024-06-07 01:46:12 +0000

    feat: adiciona configurações de limites de upload de imagens de postagens
    Signed-off-by: Rafael Coelho <rfsc.mori@gmail.com>

--- src/config/upload.config.ts
+++ src/config/upload.config.ts
@@ -8,10 +8,20 @@ export default registerAs('upload', () => {
       max_file_count: 1,
       max_file_size: 1 * 1024 * 1024, // 1 MB
     },
+
+    post_image: {
+      max_file_count: 1,
+      max_file_size: 1 * 1024 * 1024, // 1 MB
+    },
   } as const;
 
   const upload_config_schema = Joi.object({
     avatar: Joi.object({
+      max_file_count: Joi.number().integer().min(1).required(),
+      max_file_size: Joi.number().integer().min(1).required(),
+    }).required(),
+
+    post_image: Joi.object({
       max_file_count: Joi.number().integer().min(1).required(),
       max_file_size: Joi.number().integer().min(1).required(),
     }).required(),
