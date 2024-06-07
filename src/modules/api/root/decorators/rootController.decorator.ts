3bab6e9de5a8675c8680ed006a11c7ddc871b027
Rafael Coelho <rfsc.mori@gmail.com>
2024-06-07 20:48:20 +0000

    docs: corrige docs da API de exemplo no swagger
    Signed-off-by: Rafael Coelho <rfsc.mori@gmail.com>

--- src/modules/api/root/decorators/rootController.decorator.ts
+++ src/modules/api/root/decorators/rootController.decorator.ts
@@ -2,5 +2,5 @@ import { applyDecorators, Controller } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 export function rootController(): ClassDecorator {
-  return applyDecorators(ApiTags('API de exemplo.'), Controller());
+  return applyDecorators(ApiTags('API de Exemplo'), Controller());
 }
