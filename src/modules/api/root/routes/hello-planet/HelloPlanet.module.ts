9e24d831ceb9335f277dfc86527a516e25cb4257
Rafael Coelho <rfsc.mori@gmail.com>
2024-06-07 20:54:32 +0000

    feat: remove autenticação da rota de exemplo
    Signed-off-by: Rafael Coelho <rfsc.mori@gmail.com>

--- src/modules/api/root/routes/hello-planet/HelloPlanet.module.ts
+++ src/modules/api/root/routes/hello-planet/HelloPlanet.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AccessTokenAuthenticationModule } from 'modules/api/authentication/guards/access-token-authentication/AccessTokenAuthentication.module';
 
 import { HelloPlanetController } from './HelloPlanet.controller';
 import { HelloPlanetService } from './HelloPlanet.service';
 
 @Module({
-  imports: [AccessTokenAuthenticationModule],
   controllers: [HelloPlanetController],
   providers: [HelloPlanetService],
 })
