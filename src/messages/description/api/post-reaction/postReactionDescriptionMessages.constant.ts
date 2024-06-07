4b4076255192b03f2ce983bb5781dd697b19fed9
Rafael Coelho <rfsc.mori@gmail.com>
2024-06-07 15:18:33 +0000

    docs: corrige documentação da contagem de reações no swagger
    Signed-off-by: Rafael Coelho <rfsc.mori@gmail.com>

--- src/messages/description/api/post-reaction/postReactionDescriptionMessages.constant.ts
+++ src/messages/description/api/post-reaction/postReactionDescriptionMessages.constant.ts
@@ -1,3 +1,4 @@
+import type { TReactionType } from 'modules/api/post-reactions/constants/reactionType.constant';
 import { REACTION_TYPE } from 'modules/api/post-reactions/constants/reactionType.constant';
 
 export const POST_REACTION_DESCRIPTION_MESSAGES = {
@@ -7,9 +8,12 @@ export const POST_REACTION_DESCRIPTION_MESSAGES = {
   POST_REACTION_TYPE: 'Tipo da reação ao post.',
   POST_REACTIONS: 'Reações ao post.',
 
+  POST_REACTION_LIKE: 'Curtidas.',
+  POST_REACTION_DISLIKE: 'Não-curtidas.',
+
   EXAMPLE_POST_REACTION_TYPE: REACTION_TYPE.LIKE,
   EXAMPLE_POST_REACTIONS: {
-    [REACTION_TYPE.LIKE]: 1,
-    [REACTION_TYPE.DISLIKE]: 0,
-  },
+    [REACTION_TYPE.LIKE]: 7,
+    [REACTION_TYPE.DISLIKE]: 4,
+  } satisfies Record<TReactionType, number>,
 } as const;
--- src/modules/api/post-reactions/dto/PostReactionStats.dto.ts
+++ src/modules/api/post-reactions/dto/PostReactionStats.dto.ts
@@ -1,13 +1,25 @@
 import { postId } from 'modules/api/post/decorators/dto/postId.decorator';
 
 import type { TReactionType } from '../constants/reactionType.constant';
+import { postReactionDislike } from '../decorators/dto/postReactionDislike.decorator';
+import { postReactionLike } from '../decorators/dto/postReactionLike.decorator';
 import { postReactions } from '../decorators/dto/postReactions.decorator';
 import type { TPostReactionStats } from '../types/postReactionStats.type';
+
+class PostReactionStatsReactionsDto implements Record<TReactionType, number> {
+  @postReactionLike()
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  readonly LIKE: number;
+
+  @postReactionDislike()
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  readonly DISLIKE: number;
+}
 
 export class PostReactionStatsDto implements TPostReactionStats {
   @postId()
   readonly post_id: string;
 
   @postReactions()
-  readonly reactions: Readonly<Record<TReactionType, number>>;
+  readonly reactions: PostReactionStatsReactionsDto;
 }
